dfc5dea057a2b0effe9b48cc6b51470ad70be61c
ximeath <ximenaatehortuahernandez@iesantaelena.edu.co>
2023-11-16 15:23:15 -0500

    objeto json

---
+++ objeto json/data/data.js
@@ -0,0 +1,33 @@
+export const data =
+[
+    {
+        "id": 1,
+        "name": "vaca",
+        "image": "/imagenes/vaca.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "ardilla",
+        "image": "/imagenes/ardilla.webp"
+    },
+    {
+        "id": 3,
+        "name": "caballo",
+        "image": "/imagenes/caballo.jpg"
+    },
+    {
+        "id": 4,
+        "name": "erizo",
+        "image": "/imagenes/erizo.jpg"
+    },
+    {
+        "id": 5,
+        "name": "perro y gato",
+        "image": "/imagenes/p g.jpg"
+    },
+    {
+        "id": 6,
+        "name": "tortuga",
+        "image": "/imagenes/tortuga.jpg"
+    }
+]
